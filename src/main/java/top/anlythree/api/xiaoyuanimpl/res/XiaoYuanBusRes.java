c035c43c6708cdbefde78b4cd791901cb4b6da43
anlythree <wl01124501@alibaba-inc.com>
2022-03-11 15:42:37 +0800

    fix

--- src/main/java/top/anlythree/api/xiaoyuanimpl/res/XiaoYuanBusRes.java
+++ src/main/java/top/anlythree/api/xiaoyuanimpl/res/XiaoYuanBusRes.java
@@ -16,7 +16,84 @@ import java.util.List;
 @AllArgsConstructor
 public class XiaoYuanBusRes extends XiaoYuanResult{
 
-    private List<ReturlInfoRes> returlList;
+    private ReturlInfoRes returlList;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class ReturlInfoRes {
+
+        private LineInfoRes lineinfo;
+
+        private List<StationInfoRes> stations;
+
+        private List<BusInfoRes> buses;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class LineInfoRes {
+        /**
+         *  路线名称
+         */
+        private String busStaname;
+        /**
+         * 起始站
+         */
+        private String staSta;
+        /**
+         * 终点站
+         */
+        private String endSta;
+        /**
+         * 首班车时间
+         */
+        private String firTime;
+        /**
+         * 末班车时间
+         */
+        private String endTime;
+        /**
+         * 票价
+         */
+        private String busMoney;
+        /**
+         * 反向路线ID
+         */
+        private String otherLineid;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class StationInfoRes {
+
+        private String busStaname;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class BusInfoRes {
+        /**
+         * 纬度
+         */
+        private String lating;
+        /**
+         * 经度
+         */
+        private String longing;
+        /**
+         * 距离（米）
+         */
+        private String distance;
+        /**
+         * 距第几个站点（如：2，距离第2个站还有distance米，distance是0为已到站！）
+         */
+        private String disStat;
+    }
+
 
 }
 

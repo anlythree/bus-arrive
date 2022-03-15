2d030ee884080a04ff25b16c91dd790f18384a40
anlythree <wl01124501@alibaba-inc.com>
2022-03-15 19:06:07 +0800

    fix

--- src/main/java/top/anlythree/api/amapimpl/res/AMapBusRouteTimeRes.java
+++ src/main/java/top/anlythree/api/amapimpl/res/AMapBusRouteTimeRes.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- *
+ * 高德公交路线规划api返回值
  * @author anlythree
  * @description:
  * @time 2022/3/156:45 下午
@@ -15,5 +15,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AMapBusRouteTimeRes extends AMapResult{
 
+    private AMapBusRouteInfo route;
+
+    public static class AMapBusRouteInfo{
+
+        /**
+         * 起点经纬度
+         */
+        private String origin;
+
+        /**
+         * 终点经纬度
+         */
+        private String destination;
+
+        /**
+         * 公交方案列表
+         */
+        private TransitsInfo transits;
+
+        public static class TransitsInfo{
+
+        }
+
+    }
 
 }

fe1626fa6f64838615bd621d796b54958cdd9642
anlythree <15610462126@163.com>
2022-03-20 20:43:45 +0800

    fix

--- src/main/java/top/anlythree/config/SwaggerConfig.java
+++ src/main/java/top/anlythree/config/SwaggerConfig.java
@@ -41,12 +41,11 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
 
 }
